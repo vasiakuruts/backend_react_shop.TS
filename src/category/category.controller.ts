c31695eca98234c8873c1528265a8666a574f21a
KVP <vasiakuruts@gmail.com>
2024-03-25 22:13:02 +0200

    add profile to category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,14 +1,14 @@
 import {
-	Body,
-	Controller,
-	Delete,
-	Get,
-	HttpCode,
-	Param,
-	Post,
-	Put,
-	UsePipes,
-	ValidationPipe
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe
 } from '@nestjs/common'
 import { Auth } from 'src/auth/decorators/auth.decorator'
 import { CategoryService } from './category.service'
@@ -16,46 +16,43 @@ import { CategoryDto } from './dto/category.dto'
 
 @Controller('categories')
 export class CategoryController {
-	constructor(private readonly categoryService: CategoryService) {}
+  constructor(private readonly categoryService: CategoryService) {}
 
-	@Get()
-	@Auth()
-	async getAll() {
-		return this.categoryService.getAll()
-	}
+  @Get()
+  async getAll() {
+    return this.categoryService.getAll()
+  }
 
-	@Get('by-slug/:slug')
-	@Auth()
-	async getBySlug(@Param('slug') slug: string) {
-		return this.categoryService.bySlug(slug)
-	}
+  @Get('by-slug/:slug')
+  async getBySlug(@Param('slug') slug: string) {
+    return this.categoryService.bySlug(slug)
+  }
 
-	@Get(':id')
-	@Auth()
-	async getById(@Param('id') id: string) {
-		return this.categoryService.byId(+id)
-	}
+  @Get(':id')
+  async getById(@Param('id') id: string) {
+    return this.categoryService.byId(+id)
+  }
 
-	@UsePipes(new ValidationPipe())
-	@HttpCode(200)
-	@Auth()
-	@Post()
-	async create() {
-		return this.categoryService.create()
-	}
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Post()
+  async create() {
+    return this.categoryService.create()
+  }
 
-	@UsePipes(new ValidationPipe())
-	@HttpCode(200)
-	@Auth()
-	@Put(':id')
-	async update(@Param('id') id: string, @Body() dto: CategoryDto) {
-		return this.categoryService.update(+id, dto)
-	}
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() dto: CategoryDto) {
+    return this.categoryService.update(+id, dto)
+  }
 
-	@HttpCode(200)
-	@Auth()
-	@Delete(':id')
-	async delete(@Param('id') id: string) {
-		return this.categoryService.delete(+id)
-	}
+  @HttpCode(200)
+  @Auth()
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.categoryService.delete(+id)
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import {
-	BadRequestException,
-	Injectable,
-	NotFoundException
+  BadRequestException,
+  Injectable,
+  NotFoundException
 } from '@nestjs/common'
 import { Prisma } from '@prisma/client'
 import { hash } from 'argon2'
@@ -11,67 +11,68 @@ import { returnUserObject } from './object/return-user.object'
 
 @Injectable()
 export class UserService {
-	constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-	async byId(id: number, selectObject: Prisma.UserSelect = {}) {
-		const user = await this.prisma.user.findUnique({
-			where: { id },
-			select: {
-				...returnUserObject,
-				favorites: {
-					select: {
-						id: true,
-						name: true,
-						price: true,
-						images: true,
-						slug: true
-					}
-				},
-				...selectObject
-			}
-		})
+  async byId(id: number, selectObject: Prisma.UserSelect = {}) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+      select: {
+        ...returnUserObject,
+        favorites: {
+          select: {
+            id: true,
+            name: true,
+            price: true,
+            images: true,
+            slug: true,
+            category: true
+          }
+        },
+        ...selectObject
+      }
+    })
 
-		if (!user) throw new Error('User not found')
-		return user
-	}
+    if (!user) throw new Error('User not found')
+    return user
+  }
 
-	async updateProfile(id: number, dto: UserDto) {
-		const isSameUser = await this.prisma.user.findUnique({
-			where: { email: dto.email }
-		})
-		if (isSameUser && id !== isSameUser.id)
-			throw new BadRequestException('Email already in use')
+  async updateProfile(id: number, dto: UserDto) {
+    const isSameUser = await this.prisma.user.findUnique({
+      where: { email: dto.email }
+    })
+    if (isSameUser && id !== isSameUser.id)
+      throw new BadRequestException('Email already in use')
 
-		const user = await this.byId(id)
+    const user = await this.byId(id)
 
-		return this.prisma.user.update({
-			where: { id },
-			data: {
-				email: dto.email,
-				name: dto.name,
-				avatarPath: dto.avatarPath,
-				phone: dto.phone,
-				password: dto.password ? await hash(dto.password) : user.password
-			}
-		})
-	}
-	async toggleFavorite(productId: number, userId: number) {
-		const user = await this.byId(userId)
+    return this.prisma.user.update({
+      where: { id },
+      data: {
+        email: dto.email,
+        name: dto.name,
+        avatarPath: dto.avatarPath,
+        phone: dto.phone,
+        password: dto.password ? await hash(dto.password) : user.password
+      }
+    })
+  }
+  async toggleFavorite(userId: number, productId: number) {
+    const user = await this.byId(userId)
 
-		if (!user) throw new NotFoundException('User not found!')
+    if (!user) throw new NotFoundException('User not found!')
 
-		const isExists = user.favorites.some(product => product.id === productId)
+    const isExists = user.favorites.some(product => product.id === productId)
 
-		await this.prisma.user.update({
-			where: { id: user.id },
-			data: {
-				favorites: {
-					[isExists ? 'disconnect' : 'connect']: {
-						id: productId
-					}
-				}
-			}
-		})
-		return 'Success'
-	}
+    await this.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        favorites: {
+          [isExists ? 'disconnect' : 'connect']: {
+            id: productId
+          }
+        }
+      }
+    })
+    return 'Success'
+  }
 }
